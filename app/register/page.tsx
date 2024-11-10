e0f05ccaf0989aaf3e63c1ff18a2aa9c1e9f5964
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-11-10 15:17:52 +0530

    adding shadcn components

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,7 +1,19 @@
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export default function RegisterPage() {
   return (
-    <div>
-      <h1>Register Page</h1>
-    </div>
+    <main className="flex justify-center items-center min-h-screen">
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle>Register</CardTitle>
+          <CardDescription>Register for a new component</CardDescription>
+        </CardHeader>
+      </Card>
+    </main>
   );
 }
