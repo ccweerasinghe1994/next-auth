9de0c62e1ab503ae69fae2f75c7343b092ca2f2c
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-11-18 09:02:48 +0530

    enhance LogoutButton to redirect to login page after logout

--- app/(logged-in)/logout-button/LogoutButton.tsx
+++ app/(logged-in)/logout-button/LogoutButton.tsx
@@ -1,13 +1,16 @@
 "use client";
 import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 import { logout } from "./actions";
 
 export default function LogoutButton() {
+  const router = useRouter();
   return (
     <Button
       size={"sm"}
       onClick={async () => {
         await logout();
+        router.push("/login");
       }}
     >
       Logout
