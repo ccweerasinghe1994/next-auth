bbce0c2bad8d8e345a40ab845400f56b369702bc
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-11-18 16:49:13 +0530

    feat: enhance ResetPasswordPage to display success message upon email submission

--- app/(logged-out)/reset-password/page.tsx
+++ app/(logged-out)/reset-password/page.tsx
@@ -42,59 +42,73 @@ export default function ResetPasswordPage() {
 
   return (
     <main className="min-h-screen flex justify-center items-center">
-      <Card className="w-[350px]">
-        <CardHeader>
-          <CardTitle>Password Reset</CardTitle>
-          <CardDescription>
-            Enter your email address to reset your password
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <Form {...form}>
-            <form onSubmit={form.handleSubmit(handleSubmit)}>
-              <fieldset
-                disabled={form.formState.isSubmitting}
-                className="flex flex-col gap-2"
-              >
-                <FormField
-                  control={form.control}
-                  name="email"
-                  render={({ field }) => (
-                    <FormItem>
-                      <FormLabel>Email</FormLabel>
-                      <FormControl>
-                        <Input {...field} type="email" />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
+      {form.formState.isSubmitSuccessful ? (
+        <Card className="w-[380px]">
+          <CardHeader>
+            <CardTitle>Email Sent</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <CardDescription>
+              If an account with {form.getValues("email")} email exists, we sent
+              you an email with instructions on how to reset your password
+            </CardDescription>
+          </CardContent>
+        </Card>
+      ) : (
+        <Card className="w-[350px]">
+          <CardHeader>
+            <CardTitle>Password Reset</CardTitle>
+            <CardDescription>
+              Enter your email address to reset your password
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(handleSubmit)}>
+                <fieldset
+                  disabled={form.formState.isSubmitting}
+                  className="flex flex-col gap-2"
+                >
+                  <FormField
+                    control={form.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Email</FormLabel>
+                        <FormControl>
+                          <Input {...field} type="email" />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  {!!form.formState.errors.root?.message && (
+                    <FormMessage>
+                      {form.formState.errors.root?.message}
+                    </FormMessage>
                   )}
-                />
-                {!!form.formState.errors.root?.message && (
-                  <FormMessage>
-                    {form.formState.errors.root?.message}
-                  </FormMessage>
-                )}
-                <Button type="submit">Reset Password</Button>
-              </fieldset>
-            </form>
-          </Form>
-        </CardContent>
-        <CardFooter className="text-muted-foreground flex flex-col gap-2 text-sm">
-          <div>
-            Remember your password?{" "}
-            <Link className="underline" href="/login">
-              Login
-            </Link>
-          </div>
-          <div>
-            Don&apos;t have an account?
-            <Link href="/register" className="underline">
-              {" "}
-              Register
-            </Link>
-          </div>
-        </CardFooter>
-      </Card>
+                  <Button type="submit">Reset Password</Button>
+                </fieldset>
+              </form>
+            </Form>
+          </CardContent>
+          <CardFooter className="text-muted-foreground flex flex-col gap-2 text-sm">
+            <div>
+              Remember your password?{" "}
+              <Link className="underline" href="/login">
+                Login
+              </Link>
+            </div>
+            <div>
+              Don&apos;t have an account?
+              <Link href="/register" className="underline">
+                {" "}
+                Register
+              </Link>
+            </div>
+          </CardFooter>
+        </Card>
+      )}
     </main>
   );
 }
