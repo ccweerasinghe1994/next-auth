6e6a4e0944309e8d815efb261c57ae3d2f413475
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-11-18 11:31:14 +0530

    add ResetPasswordPage component for user password reset functionality

---
+++ app/(logged-out)/reset-password/page.tsx
@@ -0,0 +1,99 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { emailSchema } from "@/validation/emailSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const formSchema = z.object({
+  email: emailSchema,
+});
+
+export default function ResetPasswordPage() {
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+    },
+  });
+
+  const handleSubmit = async (data: z.infer<typeof formSchema>) => {
+    console.log(data);
+  };
+
+  return (
+    <main className="min-h-screen flex justify-center items-center">
+      <Card className="w-[350px]">
+        <CardHeader>
+          <CardTitle>Password Reset</CardTitle>
+          <CardDescription>
+            Enter your email address to reset your password
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(handleSubmit)}>
+              <fieldset
+                disabled={form.formState.isSubmitting}
+                className="flex flex-col gap-2"
+              >
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input {...field} type="email" />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                {!!form.formState.errors.root?.message && (
+                  <FormMessage>
+                    {form.formState.errors.root?.message}
+                  </FormMessage>
+                )}
+                <Button type="submit">Register</Button>
+              </fieldset>
+            </form>
+          </Form>
+        </CardContent>
+        <CardFooter className="text-muted-foreground flex flex-col gap-2 text-sm">
+          <div>
+            Remember your password?{" "}
+            <Link className="underline" href="/login">
+              Login
+            </Link>
+          </div>
+          <div>
+            Don&apos;t have an account?
+            <Link href="/register" className="underline">
+              {" "}
+              Register
+            </Link>
+          </div>
+        </CardFooter>
+      </Card>
+    </main>
+  );
+}
